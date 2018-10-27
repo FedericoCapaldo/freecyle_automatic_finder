7c3282234a76a491f27979beec83a8f26bf7bb38
Federico <federico.capaldo1@gmail.com>
2018-10-27 22:59:46 +0100

    added listed of groups I am interested in

--- gazebo_finder.py
+++ gazebo_finder.py
@@ -15,20 +15,42 @@ def get_relevantish_titles(links):
         relevant_links = list(filter(lambda link: True if "post" in link.get("href") else False, links))
         return [post for post in relevant_links if post.string is not None]
 
-def match(my_desire):
+def match(posts, my_desire):
         toRet = []
-        for p in relevant_posts:
+        for p in posts:
             p.string = p.string.lower()
             if my_desire in p.string:
                  toRet.append(p.get("href"))
         return toRet
 
+def execute_find_routine(url, wanted):
+        links = fetch_titles(url)
+        relevant_posts = get_relevantish_titles(links)
+        matched = match(relevant_posts, wanted)
+        return matched
+
+def get_group_link(name):
+    return "https://groups.freecycle.org/group/" + name + "/posts/offer"
+
+
 if __name__ == "__main__":
-    url = "https://groups.freecycle.org/group/CityOfLondon/posts/offer"
-    links = fetch_titles(url)
-    relevant_posts = get_relevantish_titles(links)
-    matched = match("gazebo")
-    print(matched)
+    group_names = [ "CityOfLondon",
+                    "LewishamUK",
+                    "GreenwichUK",
+                    "SouthwarkUK",
+                    "LambethUK",
+                    "TowerHamletsUK",
+                    "WestminsterUK",
+                    "HammersmithandFulhamUK",
+                    "errorSite"
+                    "KensingtonandChelseaUK"
+                    "HackneyUK",
+                    "EalingUK",
+                    "KentishtownUK",
+                    "BarkingandDagenham" ]
+
+    for group_name in group_names:
+        print(group_name + ": ", execute_find_routine(get_group_link(group_name), "chair"))
 
 
     # relevant_posts = [(s.lower()) for s in relevant_titles]
